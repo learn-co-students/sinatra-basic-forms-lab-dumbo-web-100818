e4f94c423caf75d7bed4275d4907bccbb4287d00
github username <github email address>
2018-10-30 16:16:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+#In app.rb build out a GET request to load a homepage. The homepage should go to the main route /.
+#index.erb
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/' do
+    @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
+
+    erb :display_puppy
+  end
 
 end
---
+++ models/puppy.rb
@@ -0,0 +1,14 @@
+#name breed age
+#attr accessor
+
+class Puppy
+
+  attr_accessor :name, :breed, :age
+
+  def initialize (name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+  
+end
